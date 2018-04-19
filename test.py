77d0dab6981ff93aee2b8c480c5572d5bff3317f
alexandder <aleksanderbolt@yahoo.com>
2018-04-20 01:35:12 +0200

    fix

--- test.py
+++ test.py
@@ -45,10 +45,10 @@ def make_graphs_for_rules(rules, sizes, layer_count):
         errors = find_errors_for_rule(r, sizes, layer_count)
         make_plot(errors, r)
 
-layer_count = 2
+layer_count = 10
 #rules = [95, 110, 150, 222]
 rules = [150]
-sizes = [1000 + 1000*x for x in range(9)]
-#sizes = [10000 + 10000*x for x in range(9)]
+sizes = [1000 + 1000*x for x in range(8)]
+#sizes = [10000 + 10000*x for x in range(8)]
 
 make_graphs_for_rules(rules, sizes, layer_count)
