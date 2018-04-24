25aa12eb362a097bf012cd6452e3d2f01538c26b
alexandder <aleksanderbolt@yahoo.com>
2018-04-24 19:12:54 +0200

    add data

--- errors.py
+++ errors.py
@@ -6,19 +6,19 @@ import numpy as np
 def find_errors_for_rule(rule, sizes, layer_count):
     errors = {}
     for m in sizes:
-        dataset = np.loadtxt('data/' + str(rule) + '/' + str(rule) + '_' + str(m) + '.txt', delimiter=",")
+        dataset = np.loadtxt('data/' + str(rule) + '/' + str(rule) + '_10000.txt', delimiter=",")
 
         model = Sequential()
-        model.add(Dense(32, input_dim=32, activation='relu'))
+        model.add(Dense(64, input_dim=32, activation='relu'))
 
         for i in range(0, layer_count):
-            model.add(Dense(32, activation='relu'))
+            model.add(Dense(64, activation='relu'))
 
         model.add(Dense(1, activation='sigmoid'))
 
         model.compile(loss='mean_squared_error', optimizer='adam')
 
-        model.fit(x = dataset[:,0:32], y = dataset[:,32], epochs=150, batch_size=32, verbose=0)
+        model.fit(x = dataset[m:,0:32], y = dataset[m:,32], epochs=450, batch_size=210, verbose=1)
         testset = np.loadtxt('data/testsets/' + str(rule) + '_test.txt', delimiter=",")
         X_test = testset[:,0:32]
         Y_test = testset[:,32]
@@ -34,8 +34,8 @@ def find_errors_for_rules(rules, sizes, layer_count):
 
 
 layer_count = 3
-rules = [r for r in range(201)]
-sizes = [1000 + 1000*x for x in range(9)]
+rules = [r for r in range(3)]
+sizes = [100 + 100*x for x in range(9)]
 
 
 print(find_errors_for_rules(rules, sizes, layer_count))
