5f78ca85b39199a69402185501e4920f81dab873
Witold Bołt <witold.bolt@jitsolutions.pl>
2018-04-29 18:47:36 +0200

    new version of the error counting script

--- errors.py
+++ errors.py
@@ -1,41 +1,50 @@
 from keras.models import Sequential
 from keras.layers import Dense
 import numpy as np
+import sys
+
+def find_error_for_rule(rule, size, layer_count, afun, epc, bs):
+    if(size>1000):
+        size=1000
+        
+    dataset = np.loadtxt('data/' + str(rule) + '/' + str(rule) + '_1000.txt', delimiter=",")
+
+    model = Sequential()
+    model.add(Dense(32, input_dim=32, activation=afun))
+
+    for _ in range(0, layer_count):
+        model.add(Dense(64, activation=afun))
+
+    model.add(Dense(1, activation='sigmoid'))
+
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    model.fit(x = dataset[0:size,0:32], y = dataset[0:size,32], epochs=epc, batch_size=bs, verbose=0)
+    testset = np.loadtxt('data/testsets2/' + str(rule) + '_test.txt', delimiter=",")
+    X_test = testset[:,0:32]
+    Y_test = testset[:,32]
+
+    predictions = model.predict(np.array(X_test))
+
+    error_vector = np.square(np.subtract(Y_test, predictions))
+    return (error_vector.min(), error_vector.mean(), error_vector.max(), error_vector.std(), np.median(error_vector))
 
 
-def find_errors_for_rule(rule, sizes, layer_count):
-    errors = {}
-    for m in sizes:
-        dataset = np.loadtxt('data/' + str(rule) + '/' + str(rule) + '_' + str(m) + '.txt', delimiter=",")
+afun = 'tanh'
+try:
+    afun = sys.argv[1]
+except:
+    pass
 
-        model = Sequential()
-        model.add(Dense(32, input_dim=32, activation='relu'))
+step=0
+try:
+    step=int(sys.argv[2])
+except:
+    pass
 
-        for i in range(0, layer_count):
-            model.add(Dense(32, activation='relu'))
-
-        model.add(Dense(1, activation='sigmoid'))
-
-        model.compile(loss='mean_squared_error', optimizer='adam')
-
-        model.fit(x = dataset[:,0:32], y = dataset[:,32], epochs=150, batch_size=32, verbose=0)
-        testset = np.loadtxt('data/testsets/' + str(rule) + '_test.txt', delimiter=",")
-        X_test = testset[:,0:32]
-        Y_test = testset[:,32]
-        predictions = model.predict(np.array(X_test))
-        errors[m] = np.square(np.subtract(Y_test, predictions)).mean()
-    return errors
-
-def find_errors_for_rules(rules, sizes, layer_count):
-    errors = {}
-    for r in rules:
-        errors[r] = find_errors_for_rule(r, sizes, layer_count)
-    return errors
-
-
-layer_count = 3
-rules = [r for r in range(201, 256)]
-sizes = [1000 + 1000*x for x in range(9)]
-
-
-print(find_errors_for_rules(rules, sizes, layer_count))
+    
+for rule in range(step*64,(step+1)*64-1):
+    for size in [100, 250, 500, 1000]:
+        for batch_size in [100, 50, 5]:
+            for epochs in [10, 50, 500, 1000]:
+                print(rule,size,batch_size,epochs,afun,find_error_for_rule(rule,size,1,afun,epochs,batch_size))
+                sys.stdout.flush()
